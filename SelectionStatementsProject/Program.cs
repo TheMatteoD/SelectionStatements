fb15daeb98095dec52b9d4bdd6823bdb3205bbba
Matteo DeSimone <thematteod@gmail.com>
2022-08-20 00:39:12 -0400

    Initial commit

---
+++ SelectionStatementsProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SelectionStatementsProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var randomNumber = random.Next();
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > randomNumber)
+            {
+                Console.WriteLine("Too High!");
+            }
+            else if (userInput < randomNumber)
+            {
+                Console.WriteLine("Too Low!");
+            }
+            else
+            {
+                Console.WriteLine("..nevermind");
+            }
+        }
+    }
+}
